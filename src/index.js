d1f72da5a1cf2cd3c3942785b0389c7c81e380c4
Lassi Chen <lachen@utu.fi>
2023-02-19 04:00:52 +0200

    t2.1 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App-1.5'
+import App from './App-2.1'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
